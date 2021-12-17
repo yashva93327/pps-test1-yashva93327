78ef6923b9898226edee18555a5f565c2e4c0e4f
yashwanth BELLALA <94678112+yashva93327@users.noreply.github.com>
2021-12-17 08:59:21 +0000

    execuited2

---
+++ p2final.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter value:\n");
+  scanf("%d",&a);
+  return a;
+}
+
+int add(int a,int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+int output(int a,int b,int sum)
+{
+  printf("sum of a b is%d+%d=%d",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  a=input();
+  b=input();
+  sum=add(a,b);
+  output(a,b,sum);
+  return 0;
+} 
