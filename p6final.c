e58b888ec37a0d3d1b6142d681eab84e17701b84
yashwanth BELLALA <94678112+yashva93327@users.noreply.github.com>
2022-01-03 06:16:26 +0000

    p6 done

---
+++ p6final.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int input_n()
+{
+  int n;
+ printf("Enter the number of terms: ");
+ scanf("%d", &n);
+ return n;
+}
+int sum_n(int n)
+{
+  int sum;
+  sum=(n*(n+1))/2;
+  return sum;
+}
+void output(int n, int sum)
+{
+  printf("The sum of first %d  natural numbers are %d", n, sum);
+}
+int main()
+{
+  int n,sum;
+  n=input_n();
+  sum=sum_n(n);
+  output(n,sum);
+  return 0;
+}
