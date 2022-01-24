4beb51eaa3900bb6c0ae7846f7432905b0559656
yashwanth BELLALA <94678112+yashva93327@users.noreply.github.com>
2022-01-24 12:23:13 +0530

    Update p8original.c

--- p8original.c
+++ p8original.c
@@ -1,66 +1,34 @@
 #include<stdio.h>
-void input_two_string(char *a,char *b)
+float input()
 {
-  printf("enter a word:");
-  scanf("%s" , a);
-  printf("enter a word:");
-  scanf("%s" , b);
-} 
-int cmp(char *a, char *b)
-{
-int i=0, result;
-while(a[i] != '\0' && b[i] != '\0')
-  {
-    if(a[i]>b[i])
-    {
-      result = -1;
-      break;
-    }
-    else if(b[i] > a[i])
-    {
-      result = 1;
-      break;
-    }
-    else
-    {
-      i++;
-    }
-  }
-  if(a[i] == '\0')
-  {
-    result = 1;
-  }
-  else if(b[i] == '\0')
-  {
-    result = -1;
-  }
-  if(a[i] =='\0' && b[i] == '\0')
-  {
-    result = 0;
-  }
-  return result;
+  float n;
+  printf("enter a number:");
+  scanf("%f" , &n);
+  return n;
 }
-void output(char *a, char *b, int ans)
+float my_sqrt(float n)
 {
-  if(ans == -1)
+  if(n==0 || n==1)
   {
-    printf("%s is greater than %s\n", a, b);
+    return n;
   }
-  else if(ans==1)
+  float i = 1,ans = 1;
+  while(ans <= n)
   {
-    printf("%s is greater than %s\n", b, a);
+    i++;
+    ans = i*i;
   }
-  else
-  {
-    printf("%s is equal to %s\n", b, a);
-  }
+  return i-1;
+}
+void output(float n, float sqrt_result)
+{
+  printf("the square root of %0.2f is :%0.2f\n",n,sqrt_result);
 }
 int main()
 {
-  char a[20],b[20];
-  int ans;
-  input_two_string(a,b);
-  ans = cmp(a, b);
-  output(a, b, ans);
+  float n,sqrt_result;
+  n=input();
+  sqrt_result = my_sqrt(n);
+  output(n, sqrt_result);
   return 0;
 }
