83a2412f626ef5e8975afae6739bdbfb0ddf0b76
yashwanth BELLALA <94678112+yashva93327@users.noreply.github.com>
2022-01-25 17:23:50 +0000

    done

--- p7final.c
+++ p7final.c
@@ -1,16 +1,43 @@
 #include<stdio.h>
+int input_size_array()
+{
+  int n;
+  printf("enter the number of inputs:");
+  scanf("%d", &n);
+  return n;
+}
+void input_array(int n, int a[n])
+{
+  for(int i=0;i<n;i++)
+  {
+    printf("enter a number:");
+    scanf("%d", &a[i]);
+  }
+}
+int sum_n_array(int n, int a[n])
+{
+  int sum=0;
+  for(int i=0;i<n;i++)
+  {
+    sum += a[i];
+  }
+  return sum;
+}
+void output(int n, int a[n], int sum)
+{
+  for(int i=0;i<n;i++)
+  {
+    printf("%d+" , a[i]);
+  }
+  printf("%d is %d\n", a[n-1],sum);
+}
 int main()
 {
-  int Number, i, Sum = 0;
-  
-  printf("\nPlease Enter any Integer Value\n");
-  scanf("%d", &Number);
-  
-  for(i = 1; i <= Number; i++)
-  {
-     Sum = Sum + i;
-  }
-  
-  printf("Sum of Natural Numbers = %d", Sum);
+  int n,sum;
+  n=input_size_array();
+  int a[n];
+  input_array(n,a);
+  sum=sum_n_array(n,a);
+  output(n,a,sum);
   return 0;
 }
